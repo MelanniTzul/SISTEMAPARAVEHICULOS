d457768f3013fa30de67b38c6855a44c6c157458
MelanniTzul <melannitzul@gmail.com>
2022-11-19 04:14:27 -0600

    Creacion de formularios y configurando visualizacion

---
+++ SistemaParaVehiculos/Principal.Designer.cs
@@ -0,0 +1,177 @@
+﻿namespace SistemaParaVehiculos
+{
+    partial class Principal
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.menuStrip = new System.Windows.Forms.MenuStrip();
+            this.vToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.cONDICIONToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.aÑOToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.dUEÑOToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.gENEROToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.mARCAToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.mODELOToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.statusStrip = new System.Windows.Forms.StatusStrip();
+            this.toolStripStatusLabel = new System.Windows.Forms.ToolStripStatusLabel();
+            this.toolTip = new System.Windows.Forms.ToolTip(this.components);
+            this.menuStrip.SuspendLayout();
+            this.statusStrip.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip
+            // 
+            this.menuStrip.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.vToolStripMenuItem,
+            this.cONDICIONToolStripMenuItem,
+            this.aÑOToolStripMenuItem,
+            this.dUEÑOToolStripMenuItem,
+            this.gENEROToolStripMenuItem,
+            this.mARCAToolStripMenuItem,
+            this.mODELOToolStripMenuItem});
+            this.menuStrip.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip.Name = "menuStrip";
+            this.menuStrip.Padding = new System.Windows.Forms.Padding(8, 3, 0, 3);
+            this.menuStrip.Size = new System.Drawing.Size(1145, 30);
+            this.menuStrip.TabIndex = 0;
+            this.menuStrip.Text = "MenuStrip";         
+            // 
+            // vToolStripMenuItem
+            // 
+            this.vToolStripMenuItem.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.vToolStripMenuItem.Name = "vToolStripMenuItem";
+            this.vToolStripMenuItem.Size = new System.Drawing.Size(96, 24);
+            this.vToolStripMenuItem.Text = "VEHICULO";
+            this.vToolStripMenuItem.Click += new System.EventHandler(this.vToolStripMenuItem_Click);
+            // 
+            // cONDICIONToolStripMenuItem
+            // 
+            this.cONDICIONToolStripMenuItem.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.cONDICIONToolStripMenuItem.Name = "cONDICIONToolStripMenuItem";
+            this.cONDICIONToolStripMenuItem.Size = new System.Drawing.Size(108, 24);
+            this.cONDICIONToolStripMenuItem.Text = "CONDICION";
+            this.cONDICIONToolStripMenuItem.Click += new System.EventHandler(this.cONDICIONToolStripMenuItem_Click);
+            // 
+            // aÑOToolStripMenuItem
+            // 
+            this.aÑOToolStripMenuItem.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.aÑOToolStripMenuItem.Name = "aÑOToolStripMenuItem";
+            this.aÑOToolStripMenuItem.Size = new System.Drawing.Size(57, 24);
+            this.aÑOToolStripMenuItem.Text = "AÑO";
+            this.aÑOToolStripMenuItem.Click += new System.EventHandler(this.aÑOToolStripMenuItem_Click);
+            // 
+            // dUEÑOToolStripMenuItem
+            // 
+            this.dUEÑOToolStripMenuItem.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.dUEÑOToolStripMenuItem.Name = "dUEÑOToolStripMenuItem";
+            this.dUEÑOToolStripMenuItem.Size = new System.Drawing.Size(76, 24);
+            this.dUEÑOToolStripMenuItem.Text = "DUEÑO";
+            this.dUEÑOToolStripMenuItem.Click += new System.EventHandler(this.dUEÑOToolStripMenuItem_Click);
+            // 
+            // gENEROToolStripMenuItem
+            // 
+            this.gENEROToolStripMenuItem.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.gENEROToolStripMenuItem.Name = "gENEROToolStripMenuItem";
+            this.gENEROToolStripMenuItem.Size = new System.Drawing.Size(83, 24);
+            this.gENEROToolStripMenuItem.Text = "GENERO";
+            this.gENEROToolStripMenuItem.Click += new System.EventHandler(this.gENEROToolStripMenuItem_Click);
+            // 
+            // mARCAToolStripMenuItem
+            // 
+            this.mARCAToolStripMenuItem.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.mARCAToolStripMenuItem.Name = "mARCAToolStripMenuItem";
+            this.mARCAToolStripMenuItem.Size = new System.Drawing.Size(78, 24);
+            this.mARCAToolStripMenuItem.Text = "MARCA";
+            this.mARCAToolStripMenuItem.Click += new System.EventHandler(this.mARCAToolStripMenuItem_Click);
+            // 
+            // mODELOToolStripMenuItem
+            // 
+            this.mODELOToolStripMenuItem.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.mODELOToolStripMenuItem.Name = "mODELOToolStripMenuItem";
+            this.mODELOToolStripMenuItem.Size = new System.Drawing.Size(86, 24);
+            this.mODELOToolStripMenuItem.Text = "MODELO";
+            this.mODELOToolStripMenuItem.Click += new System.EventHandler(this.mODELOToolStripMenuItem_Click);
+            // 
+            // statusStrip
+            // 
+            this.statusStrip.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.statusStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel});
+            this.statusStrip.Location = new System.Drawing.Point(0, 654);
+            this.statusStrip.Name = "statusStrip";
+            this.statusStrip.Padding = new System.Windows.Forms.Padding(1, 0, 19, 0);
+            this.statusStrip.Size = new System.Drawing.Size(1145, 26);
+            this.statusStrip.TabIndex = 2;
+            this.statusStrip.Text = "StatusStrip";
+            // 
+            // toolStripStatusLabel
+            // 
+            this.toolStripStatusLabel.Name = "toolStripStatusLabel";
+            this.toolStripStatusLabel.Size = new System.Drawing.Size(54, 20);
+            this.toolStripStatusLabel.Text = "Estado";
+            // 
+            // Principal
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1145, 680);
+            this.Controls.Add(this.statusStrip);
+            this.Controls.Add(this.menuStrip);
+            this.IsMdiContainer = true;
+            this.MainMenuStrip = this.menuStrip;
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "Principal";
+            this.Text = "Principal";
+            this.menuStrip.ResumeLayout(false);
+            this.menuStrip.PerformLayout();
+            this.statusStrip.ResumeLayout(false);
+            this.statusStrip.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+        #endregion
+
+
+        private System.Windows.Forms.MenuStrip menuStrip;
+        private System.Windows.Forms.StatusStrip statusStrip;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel;
+        private System.Windows.Forms.ToolTip toolTip;
+        private ToolStripMenuItem vToolStripMenuItem;
+        private ToolStripMenuItem cONDICIONToolStripMenuItem;
+        private ToolStripMenuItem aÑOToolStripMenuItem;
+        private ToolStripMenuItem dUEÑOToolStripMenuItem;
+        private ToolStripMenuItem gENEROToolStripMenuItem;
+        private ToolStripMenuItem mARCAToolStripMenuItem;
+        private ToolStripMenuItem mODELOToolStripMenuItem;
+    }
+}
+
+
+
---
+++ SistemaParaVehiculos/Principal.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SistemaParaVehiculos
+{
+    public partial class Principal : Form
+    {
+        private int childFormNumber = 0;
+
+        public Principal()
+        {
+            InitializeComponent();
+        }
+
+        private void ShowNewForm(object sender, EventArgs e)
+        {
+            Form childForm = new Form();
+            childForm.MdiParent = this;
+            childForm.Text = "Ventana " + childFormNumber++;
+            childForm.Show();
+        }
+
+        private void OpenFile(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.Personal);
+            openFileDialog.Filter = "Archivos de texto (*.txt)|*.txt|Todos los archivos (*.*)|*.*";
+            if (openFileDialog.ShowDialog(this) == DialogResult.OK)
+            {
+                string FileName = openFileDialog.FileName;
+            }
+        }
+
+        private void SaveAsToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            saveFileDialog.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.Personal);
+            saveFileDialog.Filter = "Archivos de texto (*.txt)|*.txt|Todos los archivos (*.*)|*.*";
+            if (saveFileDialog.ShowDialog(this) == DialogResult.OK)
+            {
+                string FileName = saveFileDialog.FileName;
+            }
+        }
+
+        private void ExitToolsStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void CutToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void CopyToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void PasteToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void ToolBarToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void StatusBarToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void CascadeToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            LayoutMdi(MdiLayout.Cascade);
+        }
+
+        private void TileVerticalToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            LayoutMdi(MdiLayout.TileVertical);
+        }
+
+        private void TileHorizontalToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            LayoutMdi(MdiLayout.TileHorizontal);
+        }
+
+        private void ArrangeIconsToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            LayoutMdi(MdiLayout.ArrangeIcons);
+        }
+
+        private void vToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Form1 form1Vehiculo = new Form1();//creamos una variable y llamamos alformualrio que queremos mostrar 
+            form1Vehiculo.MdiParent = this;//que se ingrente dentro de MdiParent
+            form1Vehiculo.Show();//mostramos el formulario
+
+        }
+
+        private void cONDICIONToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Form2 form2Condicion = new Form2();//creamos una variable y llamamos alformualrio que queremos mostrar 
+            form2Condicion.MdiParent = this;//que se ingrente dentro de MdiParent
+            form2Condicion.Show();//mostramos el formulario
+        }
+
+        private void aÑOToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Form3 form3year = new Form3();//creamos una variable y llamamos alformualrio que queremos mostrar 
+            form3year.MdiParent = this;//que se ingrente dentro de MdiParent
+            form3year.Show();//mostramos el formulario
+        }
+
+        private void dUEÑOToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Form4 form4Dueno = new Form4();
+            form4Dueno.MdiParent = this;
+            form4Dueno.Show();
+        }
+
+        private void gENEROToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Form5 form5Genero= new Form5();
+            form5Genero.MdiParent = this;
+            form5Genero.Show();
+        }
+
+        private void mARCAToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Form6 form6Marca= new Form6();
+            form6Marca.MdiParent = this;
+            form6Marca.Show();
+        }
+
+        
+
+        private void mODELOToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Form7 form7Modelo = new Form7();
+            form7Modelo.MdiParent = this;
+            form7Modelo.Show();
+        }
+    }
+}
+
